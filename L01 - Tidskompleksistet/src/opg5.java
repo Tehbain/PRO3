c977bcc8ae90c3c1e7b6bd4fdda408e9a2cf7296
Lucas <lhasselgaard@gmail.com>
2024-02-06 00:28:50 +0100

    opg5 done

--- L01 - Tidskompleksistet/src/opg5.java
+++ L01 - Tidskompleksistet/src/opg5.java
@@ -1,3 +1,6 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class opg5 {
 
     /*
@@ -15,13 +18,25 @@ public class opg5 {
         Hvad er størrelsesordenen af tidskompleksiteten for metoderne?
      */
 
+    public static void main(String[] args) {
+        Nedboer nedbør = new Nedboer();
+        System.out.println();
+        System.out.println("Bedste tre ferieuger starter fra uge " + nedbør.bedsteTreFerieUger());
+        System.out.println();
+
+        int antalUger = 4;
+        System.out.println("Bedste " + antalUger + " ferieuger starter fra uge: " + nedbør.bedsteFerieUgerStart(antalUger));
+        System.out.println();
+
+        System.out.println("Ugen, der indleder flest forekomster af samme nedbør: " + nedbør.ensNedboer());
+    }
     
-    
-    private class Nedboer {
-        private int[] nedboerPrUge = { 20, 10, 12, 12, 13, 14, 15, 10, 8, 7, 13,
-                                        15, 10, 9, 6, 8, 12, 22, 14, 16, 16, 18, 23, 12, 0, 2, 0, 0, 78, 0,
-                                        0, 0, 34, 12, 34, 23, 23, 12, 44, 23, 12, 34, 22, 22, 22, 22, 18,
-                                        19, 21, 32, 24, 13 };
+    private static class Nedboer {
+        private int[] nedboerPrUge = { 20, 10, 12, 12, 13, 14, 15, 10, 8, 7, 13, 15,
+                                       10, 9, 6, 8, 12, 22, 14, 16, 16, 18, 23, 12,
+                                       0, 2, 0, 0, 78, 0, 0, 0, 34, 12, 34, 23,
+                                       23, 12, 44, 23, 12, 34, 22, 22, 22, 22, 18, 19,
+                                       21, 32, 24, 13 };
 
         /**
          * Returnerer ugenummeret for den uge i året, hvor man skal starte ferien,
@@ -30,15 +45,17 @@ public class opg5 {
          * @return
          */
 
-        public int bedsteTreFerieUger() {
-            
-            int best = 0;
-
+        public int bedsteTreFerieUger() { //O(1)
+            int best = nedboerPrUge[0] + nedboerPrUge[1] + nedboerPrUge[2];
+            int vacationStart = 0;
             for (int i = 0; i < nedboerPrUge.length - 4; i++) {
-                
+                int threeWeeks = nedboerPrUge[i] + nedboerPrUge[i+1] + nedboerPrUge[i+2];
+                if (threeWeeks < best) {
+                    best = threeWeeks;
+                    vacationStart = i+1;
+                }
             }
-            
-            return -1;
+            return vacationStart;
         }
 
         /**
@@ -49,10 +66,29 @@ public class opg5 {
          * @return
          */
 
-        public int bedsteFerieUgerStart(int antal) {
-            // TODO return -1 kun indsat så der ikke er compile fejl
-            return -1;
-        }
+        public int bedsteFerieUgerStart(int antal) { //O(n^2)
+
+            int[][] weeksPrecip = new int[nedboerPrUge.length - 1 - antal][1];
+
+            for (int i = 0; i < nedboerPrUge.length - 1 - antal; i++) {
+                int sum = 0;
+                for (int j = 0; j < antal; j++) {
+                    sum += nedboerPrUge[i+j];
+                }
+                weeksPrecip[i][0] = sum;
+            }
+
+            int best = Short.MAX_VALUE;
+            int vacationStart = 0;
+            for (int i = 0; i < nedboerPrUge.length - 1 - antal; i++) {
+                if (weeksPrecip[i][0] < best) {
+                    best = weeksPrecip[i][0];
+                    vacationStart = i+1;
+                }
+            }
+            return vacationStart;
+        } 
+        
 
         /**
          * Returnerer ugenummeret på den første uge hvor nedbøren har været præcis
@@ -61,8 +97,24 @@ public class opg5 {
          * @return
          */
         public int ensNedboer() {
-            // TODO return -1 kun indsat så der ikke er compile fejl
-            return -1;
+            int repeats = 0;
+            int foundWeek = -1;
+
+            for (int i = 0; i < nedboerPrUge.length - 2; i++) {
+                repeats = 0;
+                if (nedboerPrUge[i] == nedboerPrUge[i+1]) {
+                    boolean sameSame = true;
+                    int j = i+1;
+                    while (sameSame) {
+                        if (nedboerPrUge[i] == nedboerPrUge[j]) {
+                            repeats++;
+                            foundWeek = i+1 - repeats;
+                            j++;
+                        } else sameSame = false;
+                    }
+                }
+            }
+            return foundWeek;
         }
     }
 }
